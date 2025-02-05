dceaca6b69b9be7e5d7c697947fa53b971b2695d
Killar.exe <killar.exe@gmail.com>
2025-02-05 22:55:55 +0100

    Add Neopixels

--- memory_learn.ino
+++ memory_learn.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
 
 #include "memory_learn.hpp"
 #include "buzzer_driver.hpp"
@@ -7,6 +8,7 @@
 
 MemoryLearn memory_learn;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+Adafruit_NeoPixel leds(8, LEDS_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   lcd.init();
@@ -15,6 +17,8 @@ void setup() {
   lcd.createChar(1, BOTTOM_LEFT_ARROW);
   lcd.createChar(2, TOP_RIGHT_ARROW);
   lcd.createChar(3, BOTTOM_RIGHT_ARROW);
+
+  leds.begin();
 
   init_buzzer_driver(&memory_learn.buzzer);
   play_buzzer_driver(&memory_learn.buzzer, LED_MUSIC);
